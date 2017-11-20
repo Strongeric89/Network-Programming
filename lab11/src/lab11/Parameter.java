d58755fac186fffc84eb3da00fb35f1edbc95862
eric <ericstrong89@gmail.com>
2017-11-20 21:31:12 +0000

    lab 11 - jsp

---
+++ lab11/src/lab11/Parameter.java
@@ -0,0 +1,49 @@
+package lab11;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * 2. Write a program to pass parameter from URL to server using Java Servlet and
+open the webpage on basis of that parameter value for e.g.
+ */
+@WebServlet("/Parameter")
+public class Parameter extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Parameter() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		PrintWriter out = response.getWriter();
+		out.println("Lab 11 - Network Programming");
+		String arg1 = request.getParameter("page");
+		response.sendRedirect(arg1);
+		
+		
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
